ab05b85404b5010a2877a8f9c4ceffe6b39c17d0
CyNEXX <cynexx@gmail.com>
2020-02-29 20:08:32 +0200

    - added POJOs. Closes #2

---
+++ src/main/java/cynexx/springframework/cnxpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package cynexx.springframework.cnxpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/cynexx/springframework/cnxpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package cynexx.springframework.cnxpetclinic.model;
+
+public class Vet extends Person{
+}
