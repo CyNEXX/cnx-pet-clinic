cf06eb71f9f6f44493b5f833468bf33822f1b920
CyNEXX <cynexx@gmail.com>
2020-02-29 15:30:50 +0200

    Initial project

---
+++ src/main/java/cynexx/springframework/cnxpetclinic/CnxPetClinicApplication.java
@@ -0,0 +1,13 @@
+package cynexx.springframework.cnxpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnxPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnxPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cynexx/springframework/cnxpetclinic/CnxPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package cynexx.springframework.cnxpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnxPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
