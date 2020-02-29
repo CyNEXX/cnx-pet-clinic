182b50128dd025b780b70db923b945db13b59c16
CyNEXX <cynexx@gmail.com>
2020-03-01 01:47:12 +0200

    - refactored to multi-module mvn project. Closes #1

--- pet-clinic-web/src/main/java/cynexx/springframework/cnxpetclinic/CnxPetClinicApplication.java
+++ pet-clinic-web/src/main/java/cynexx/springframework/cnxpetclinic/CnxPetClinicApplication.java
@@ -3,6 +3,7 @@ package cynexx.springframework.cnxpetclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CnxPetClinicApplication {
 
